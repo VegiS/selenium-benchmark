436eb558557f91b774a2f05859789d7afb6464f4
Dave Haeffner <dhaeffner@gmail.com>
2016-08-14 12:34:33 -0400

    General cleanup. Added looping over page objects for full benchmark data per browser. And added exceptions when a browser version is not provided or for if a local server instance isn't running.

--- lib/selenium-benchmark.rb
+++ lib/selenium-benchmark.rb
@@ -1,27 +1,35 @@
 require_relative 'page-objects/small_dom'
 require_relative 'page-objects/large_dom'
+require 'rest-client'
 
-ENV['target'] ||= 'large'
 ENV['iterations'] ||= '100'
 ENV['browser'] ||= 'firefox'
-ENV['browser_version'] ||= '1'
+raise 'No browser version specified' unless ENV['browser_version']
 ENV['base_url'] = 'http://localhost:4567'
-ENV['log_write'] = 'on'
+TARGETS = [ 'large', 'small' ]
+begin
+  server_up = RestClient.head(ENV['base_url']).code == 200
+rescue Errno::ECONNREFUSED
+  raise 'Server not running! Make sure you have a local copy of https://github.com/tourdedave/the-internet running.' unless server_up
+end
 
-if ENV['log_write'] == 'on'
-  current_dir = File.join(File.dirname(File.expand_path(__FILE__)))
-  $stdout = File.new("#{current_dir}/../benchmarks/#{ENV['browser']}_#{ENV['browser_version']}.log", 'w')
+if ENV['browser'] == 'chrome'
+  Selenium::WebDriver::Chrome::Service.executable_path = File.join(Dir.pwd, 'vendor/chromedriver/2.23/chromedriver') 
+end
+
+current_dir = File.join(File.dirname(File.expand_path(__FILE__)))
+
+TARGETS.each do |target|
+  driver = Selenium::WebDriver.for ENV['browser'].to_sym
+  $stdout = File.new(
+              "#{current_dir}/../benchmarks/#{ENV['browser']}_#{ENV['browser_version']}_#{target}.log",
+              'w')
   $stdout.sync = true
+  if target == 'small'
+    page = SmallDOM.new(driver)
+  else
+    page = LargeDOM.new(driver)
+  end
+  page.benchmark
+  driver.quit
 end
-
-driver = Selenium::WebDriver.for ENV['browser'].to_sym
-case ENV['target']
-when 'small'
-  tables = SmallDOM.new(driver)
-  tables.benchmark
-when 'large'
-  large = LargeDOM.new(driver)
-  large.benchmark
-end
-
-driver.quit
